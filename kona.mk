3df57c5e30b41e23c9aa8f893c31bde0b525b427
V1t0rrr <pondebatata@outlook.com>
2022-05-17 23:48:53 -0300

    sm8250-common: copy android.hardware.fingerprint.xml

--- kona.mk
+++ kona.mk
@@ -165,3 +165,7 @@ PRODUCT_PACKAGES += \
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
+
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
