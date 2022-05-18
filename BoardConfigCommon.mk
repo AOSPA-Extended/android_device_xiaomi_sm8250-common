92b84bdaa293205f32e0374eb372a679d357a713
V1t0rrr <pondebatata@outlook.com>
2022-05-17 22:56:05 -0300

    sm8250-common: define Arch flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -16,3 +16,15 @@ COMMON_PATH := device/xiaomi/sm8250-common
 
 # Inherit the proprietary files
 include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a76
