46dced4f7753641ea49f89c12aa75fb025a0b605
SahilSonar <sss.sonar2003@gmail.com>
2022-05-18 00:11:14 -0300

    sm8250-common: build vndservicemanager

--- kona.mk
+++ kona.mk
@@ -246,3 +246,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
+
+# Vendor Service Manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
